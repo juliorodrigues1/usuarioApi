e111c83362c8b8488c0e48df42bf3e72c8900639
Julio Cesar <juliorodirgues.ac@gmail.com>
2023-06-09 14:30:57 -0500

    configurando autenticação e autorização

--- UsuarioApi/Program.cs
+++ UsuarioApi/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using UsuarioApi.Authorization;
 using UsuarioApi.Data;
 using UsuarioApi.Models;
 using UsuarioApi.Services;
@@ -27,6 +32,26 @@ namespace UsuarioApi
 
             builder.Services.AddScoped<UsuarioService>();
             builder.Services.AddScoped<TokenService>();
+            builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
+            builder.Services.AddAuthentication(opts =>
+            {
+                opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("kasjdklasndalskdnaldknaldknafjhdjfljksdjflskdfj")),
+                    ValidateIssuer = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            builder.Services.AddAuthorization(opts =>
+            {
+                opts.AddPolicy("IdadeMinima", policy => 
+                    policy.AddRequirements(new IdadeMinima(18))
+                    );
+            });
             
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -43,9 +68,8 @@ namespace UsuarioApi
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
